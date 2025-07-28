da8833ae42e31d6d18f83d32d81d6066e4124768
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-07-28 12:30:30 +0100

    add laravel sanctum and breeze, remove unused files

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
-Route::get('/home', function () {
-    return 'welcome home';
-});
+
+require __DIR__.'/auth.php';
