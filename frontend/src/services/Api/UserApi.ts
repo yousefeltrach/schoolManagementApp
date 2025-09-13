9720b78a05f4c90d3c4d20850dfffbd320c39364
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-09-13 18:27:32 +0100

    add user api service for authentication

---
+++ frontend/src/services/Api/UserApi.ts
@@ -0,0 +1,14 @@
+import {axiosClient} from "../../api/axios.js";
+
+const UserApi = {
+  login: async (email, password) => {
+    return await axiosClient.post('/login', {email, password})
+  },
+  logout: async () => {
+    return await axiosClient.post('/logout')
+  },
+  getUser: async () => {
+    return await axiosClient.get('/me')
+  },
+}
+export default UserApi
